f0001b1e16ed054a242fc80f596d6be818447e35
gilbe <gilbe@DESKTOP-BO1ES6V>
2022-03-21 15:05:20 -0600

    Agregar archivos de proyecto.

---
+++ Pages/contacto.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace gamers_crc.Pages
+{
+    public class contactoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
